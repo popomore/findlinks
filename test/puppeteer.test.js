eb65f6aa3570a2accea1db48f19c56c33179f54f
William.Tung <maledong_public@foxmail.com>
2023-06-06 16:22:47 +0800

    feat: Expose puppteerRenderingTimeout (#9)
    Sometimes when this lib is runnin for Egg in Node 20, there'll be an
    error saying "Timeout Exceed 10000 seconds", so we are NOT sure how much
    time it will last, this feat will extend the lib to make it more
    flexible.

--- test/puppeteer.test.js
+++ test/puppeteer.test.js
@@ -18,7 +18,7 @@ describe('test/puppeteer.test.js', () => {
   after(done => server.close(done));
 
   it('should get result', async () => {
-    const result = await findlinks({ src: 'http://127.0.0.1:3000', puppeteer, logger: console });
+    const result = await findlinks({ src: 'http://127.0.0.1:3000', puppeteer, logger: console, puppeteerRenderTimeout: 15000 });
     assert.deepEqual(result, {
       count: 76,
       success: 74,
